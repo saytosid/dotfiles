c94b7888deccd497b80816dc4c0278d335b58fcb
Siddhant Kumar <saytosid@gmail.com>
2019-12-15 11:49:39 +0000

    i3pystatus and xbindkeysrc

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@ ln -s `realpath zshrc`  ~/.zshrc
 ln -s `realpath tmux.conf` ~/.tmux.conf
 ln -s `realpath vimrc`  ~/.vimrc
 ln -s `realpath i3-config` ~/.config/i3/config 
-ln -s `realpath i3blocks-config` ~/.i3blocks.conf
+ln -s `realpath i3pystatus.conf` ~/.config/i3/i3pystatus.conf
