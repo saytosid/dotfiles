f03055b8233b12d6f9ce32b0fc41eaee7a07a7a9
Siddhant Kumar <saytosid@gmail.com>
2023-06-27 23:40:11 +0100

    update 2023-06-27

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
-ln -s `realpath zshrc`  ~/.zshrc
+#ln -s `realpath zshrc`  ~/.zshrc
 ln -s `realpath tmux.conf` ~/.tmux.conf
 ln -s `realpath vimrc`  ~/.vimrc
-ln -s `realpath i3-config` ~/.config/i3/config 
-ln -s `realpath i3pystatus.conf` ~/.config/i3/i3pystatus.conf
+ln -s `realpath bashrc`  ~/.bashrc
+#ln -s `realpath i3-config` ~/.config/i3/config 
+#ln -s `realpath i3pystatus.conf` ~/.config/i3/i3pystatus.conf
